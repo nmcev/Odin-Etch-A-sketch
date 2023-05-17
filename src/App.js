76e45fb10a2416d52259897cc797070ad72d3430
muha <1student.muhammed1@gmail.com>
2023-05-17 17:47:18 +0300

    chore : add reset  + random color func

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
+let isDrawingEnabled = true; // Variable to track the drawing state
+let selectColor = "#000"; // Default color for drawing
+
 window.addEventListener('DOMContentLoaded', () => {
-    createBoard();
-    getSize();
+    createBoard(16);
+    // Create a new board
+    let selectSize = document.getElementById('sel');
+    selectSize.addEventListener('click', function (e) {
+        let size = getSize();
+        createBoard(size);
+    });
+
+    // reset  board
+    let resetBtn = document.getElementById('reset');
+    resetBtn.addEventListener('click', function () {
+        resetBoard();
+    });
+
+    // toggle random color
+    let randColorBtn = document.getElementById('rand-color');
+    randColorBtn.addEventListener('click', function () {
+        generateRandomColor();
+    });
+
 });
 
-
-// to create a new board
+// To create a new board
 function createBoard(size) {
     const board = document.getElementById('board');
+    board.innerHTML = ''; // Clear the board before creating a new one
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let numDiv = size * size;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div");
 
-    for (let i = 0; i < numDiv; i++) {
+            div.addEventListener("mouseover", function () {
+                if (isDrawingEnabled) {
+                    div.style.backgroundColor = selectColor;
+                }
+            });
 
-        let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend", div);
+            board.appendChild(div);
+        }
     }
 }
 
-// getting size of board 
+// Getting size of board
 function getSize() {
-    let userInput = prompt("Enter your board size ");
+    let userInput = prompt("Enter your board size");
     let message = document.getElementById("message");
 
-    if (userInput == "") {
-        message.innerHTML = "Please enter a number";
+    if (userInput === "") {
+        message.textContent = "Please enter a number";
+    } else if (userInput < 0 || userInput > 100) {
+        message.textContent = "Please enter a number between 0 and 100";
+    } else {
+        message.textContent = "Your board size is: " + userInput + "x" + userInput;
+        return userInput;
     }
-    else if (userInput < 0 || userInput > 100) {
-        message.innerHTML = "Please enter a number between 0 and 100";
-    }
-    else {
-        message.innerHTML = "Your board size is: " + userInput + "x" + userInput;
-    }
+}
+
+// Reset the board
+function resetBoard() {
+    const board = document.getElementById('board');
+    const divs = board.querySelectorAll('div');
+    divs.forEach((div) => {
+        div.style.backgroundColor = '';
+    });
+    clearInterval(intervalId); // Clear the animation interval
+}
+
+// Generate a random color
+function generateRandomColor() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    selectColor = randomColor;
 }
