f2c748d24502999d92fdf37272db681026e51dff
muha <1student.muhammed1@gmail.com>
2023-05-17 09:28:10 +0300

    chore : first commit +add custom font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Lexend': ['Lexend','sans-serif','Sigmar', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
